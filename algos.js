6ce69a28f6736537c09e8022427912dda5b981fd
ConnorMul <mulhollandconnor@icloud.com>
2021-04-07 13:08:05 -0400

    binary search

--- algos.js
+++ algos.js
@@ -397,3 +397,23 @@ function shiftAndUpdate(threeLargest, num, idx) {
 		}
 	}
 }
+
+// BINARY SEARCH
+
+function binarySearch(array, target) {
+    return binarySearchHelper(array, target, 0, array.length - 1)
+  }
+  
+  function binarySearchHelper(array, target, left, right) {
+      if (left > right) return -1 
+      
+      const mid = Math.floor((left + right) / 2)
+      if (target === array[mid]) {
+          return mid
+      } else if (target < array[mid]) {
+          return binarySearchHelper(array, target, left, mid - 1)
+      } else if (target > array[mid]) {
+          return binarySearchHelper(array, target, mid + 1, right)
+      }
+      
+  }
