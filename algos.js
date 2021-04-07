b1ebd27da545b972e074edb9c3309cc16e073428
ConnorMul <mulhollandconnor@icloud.com>
2021-04-07 18:51:25 -0400

    new problem

--- algos.js
+++ algos.js
@@ -538,3 +538,19 @@ var maximumWealth = function(accounts) {
     
     return greatestCustomerWealth
 }
+
+// NUM OF GOOD PAIRS (LEETCODE)
+
+var numIdenticalPairs = function(nums) {
+    let numOfGoodPairs = 0
+    
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] === nums[j] && i < j) {
+                numOfGoodPairs++
+            }
+        }
+    }
+    
+    return numOfGoodPairs
+};
