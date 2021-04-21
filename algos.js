bb409de9a8b584aa1268d16d9d5cadd57270cb74
ConnorMul <mulhollandconnor@icloud.com>
2021-04-21 11:35:22 -0400

    new problem

--- algos.js
+++ algos.js
@@ -771,3 +771,17 @@ var mergeAlternately = function(word1, word2) {
     
     return resultArr.join("")
 };
+
+// NUM OF STUDENTS DOING HW AT A CERTAIN TIME (LEETCODE)
+
+var busyStudent = function(startTime, endTime, queryTime) {
+    let numOfStudentsDoingHWAtQueryTime = 0
+    
+    for (let i = 0; i < startTime.length; i++) {
+        if (queryTime >= startTime[i] && queryTime <= endTime[i]) {
+            numOfStudentsDoingHWAtQueryTime++
+        }
+    }
+    
+    return numOfStudentsDoingHWAtQueryTime
+};
