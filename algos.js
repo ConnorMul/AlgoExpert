6334d3e11a07b2c38880d278b9ee859d1549c974
ConnorMul <mulhollandconnor@icloud.com>
2021-05-27 11:37:49 -0400

    new problem

--- algos.js
+++ algos.js
@@ -1048,3 +1048,29 @@ var countSubstrings = function(s) {
 var groupAnagrams = function(strs) {
     
 };
+
+// THREE NUM SUM
+
+function threeNumberSum(array, targetSum) {
+	array.sort((a, b) => a - b)
+  const resultsArr = []
+	
+	for (let i = 0; i < array.length; i++) {
+		let left = i + 1
+		let right = array.length - 1
+		while (left < right) {
+			const currentSum = array[i] + array[left] + array[right]
+			if (currentSum === targetSum) {
+				resultsArr.push([array[i], array[left], array[right]])
+				left++
+				right--
+			} else if (currentSum < targetSum) {
+				left++
+			} else if (currentSum > targetSum) {
+				right--
+			}
+		}
+	}
+	
+	return resultsArr
+}
